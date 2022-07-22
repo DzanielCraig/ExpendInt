9b06e75753d7aa00637edebe8c6b26c223bb28f4
Dong Z <EGZhaoDong@outlook.com>
2022-07-22 11:52:06 +0800

    master

--- cint.h
+++ cint.h
@@ -3,14 +3,14 @@
 // Weather:sunny
 
 #include <iostream>
-#define MinDigit 18
+#define MinPrec 18
 using namespace std;
 
 // now we want to expend int,which can slove number that bigger than long long int
 
 // new idea:
 // 1.replace unsigned int with char
-// 2.replace signal digit with double、treble...
+// 2.replace signal precision with double、treble...
 
 // now we have DataStruct like cint,and operator like:
 class cint
@@ -20,7 +20,8 @@ private:
     long long int N;
 
     char sign;
-    int digit;
+    int afterPoint; // after decimal point
+    int precision;
     int capacity;
 
 public:
@@ -40,7 +41,7 @@ inline cint::cint() // initalize with both none
     number = NULL;
     N = 0;
     sign = '+';
-    digit = 0;
+    precision = 0;
     capacity = 0;
 }
 
@@ -50,7 +51,7 @@ inline cint::~cint()
     number = NULL;
     N = 0;
     sign = '+';
-    digit = 0;
+    precision = 0;
     capacity = 0;
 }
 
@@ -58,7 +59,7 @@ inline cint::cint(const cint &item)
 {
     N = item.N;
     sign = item.sign;
-    digit = item.digit;
+    precision = item.precision;
     capacity = item.capacity;
 
     if (item.number == NULL)
@@ -66,22 +67,22 @@ inline cint::cint(const cint &item)
     else
     {
         number = new unsigned int[capacity]{0};
-        for (int i = 0; i < digit; i++)
+        for (int i = 0; i < precision; i++)
             number[i] = item.number[i];
     }
 }
-int neighbor(int digit) // find new capacity
+int neighbor(int precision) // find new capacity
 {
     int ans = 1, i;
-    if (digit == 0)
+    if (precision == 0)
         ans = 0;
     else
-        for (i = 0; i < digit; i++)
+        for (i = 0; i < precision; i++)
             ans *= 10;
     return ans;
 }
 
-int Digit(int x) // get x's digit
+int Digit(int x) // get x's precision
 {
     int ans = 1;
     while ((x /= 10) != 0)
@@ -100,16 +101,16 @@ int Strlen(char *A) // get A's length
 char *
 Itoa(int x, char *A = NULL) // let x follow *A
 {
-    int digit = Digit(x), i;
+    int precision = Digit(x), i;
     if (A == NULL)
-        char *A = new char[digit + 1];
-    for (i = digit - 1; i >= 0 && A != NULL; i--)
+        char *A = new char[precision + 1];
+    for (i = precision - 1; i >= 0 && A != NULL; i--)
     {
         A[i] = (char)(x % 10 + '0');
         x /= 10;
     }
     if (A != NULL)
-        A[digit] = '\0';
+        A[precision] = '\0';
     return A;
 }
 
@@ -136,49 +137,13 @@ bool IsCintPart(char ch)
 ostream &
 operator<<(ostream &outPut, const cint &orig)
 {
-    for (int i = 0; i < orig.digit; i++)
+    for (int i = 0; i < orig.precision; i++)
         outPut << orig.number[i];
 
     return outPut;
 }
 
-// Created by EGzhaodong@outlook.com
-// 2022 | 星期四 | 09:20:17
-// Weather:sunny
-void cint::Modify(int max = 0) // let number array become the right format
-{
-    if (max == 0)
-        max = capacity;
-    if (max > capacity)
-    {
-        unsigned int *T = number;
-        number = new unsigned int[neighbor(max)]{0};
-        for (int i = 0; i < digit; i++)
-            number[i] = T[i];
-        delete[] T;
-        N = 0;
-    }
 
-    int i, x = max - digit;
-    for (i = max - 1; i >= x; i--) // move array to right side
-    {
-        number[i] = number[i - x];
-        number[i - x] = 0;
-    }
-
-    for (i = max - 1; i > 0; i--) // carry digit
-        if (number[i] > 9)
-        {
-            number[i - 1] += number[i] / 10;
-            number[i] = number[i] % 10;
-        }
-    for (x = 0; x < max; x++) // find first digit
-        if (number[x] != 0)
-            break;
-    for (i = x; i < max; i++) // move array to left side
-        number[i - x] = number[i];
-    digit = max - x;
-}
 
 // Created by EGzhaodong@outlook.com
 // 2022 | 星期四 | 09:21:11
@@ -186,7 +151,7 @@ void cint::Modify(int max = 0) // let number array become the right format
 cint cint::operator+(const cint &item)
 {
     cint ans(*this);
-    int maxDigit = digit > item.digit ? digit : item.digit;
+    int maxDigit = precision > item.precision ? precision : item.precision;
     for (int i = 0; i < maxDigit; i++)
     {
     }
